34005be91ba88ccdf56da93b8453f763e84244a3
Sambego <sambellen@gmail.com>
2020-09-07 16:53:17 +0200

    Show a warning on both the homepage and debugger when registering with an authenticator that does not enforce a user presence test

--- src/tutorial/step-3.js
+++ src/tutorial/step-3.js
@@ -1,59 +1,66 @@
 import Animation from "./animation.js";
 import { scrollTo, loadStep, activateStep, onObjectLoad } from "./utils.js";
 import * as step4 from "./step-4.js";
-import { credentialsGetPublicKeyJWK } from "./webauthn.js";
+import { credentialsGetPublicKeyJWK, isUserPresent } from "./webauthn.js";
 import { binToHex } from "../debugger/transformations.js";
 
 let animation;
 let rawId;
 
 const dom = {
-  object: document.getElementById("tutorial-step-3-animation-object"),
-  rawId: document.getElementById("tutorial-step-3-data-raw-id"),
-  publicKey: document.getElementById("tutorial-step-3-data-public-key")
+    object: document.getElementById("tutorial-step-3-animation-object"),
+    rawId: document.getElementById("tutorial-step-3-data-raw-id"),
+    publicKey: document.getElementById("tutorial-step-3-data-public-key"),
 };
 
 export async function trigger(credentials) {
-  rawId = credentials.rawId;
+    rawId = credentials.rawId;
 
-  const publicKey = JSON.stringify(
-    credentialsGetPublicKeyJWK(credentials),
-    null,
-    2
-  ).replace(/\{\n|\n\}|  /g, "");
+    const publicKey = JSON.stringify(
+        credentialsGetPublicKeyJWK(credentials),
+        null,
+        2
+    ).replace(/\{\n|\n\}|  /g, "");
 
-  dom.rawId.textContent = binToHex(rawId);
-  dom.publicKey.textContent = publicKey;
-
-  animation.run({
-    check: {
-      translateX: Animation.offsets.check.center,
-      opacity: 1
+    const userPresent = isUserPresent(credentials);
+    if (!userPresent) {
+        document
+            .querySelector(".tutorial-step-3-up-warning")
+            .classList.remove("hidden");
     }
-  });
 
-  scrollTo(".tutorial-step-3-container");
-  activateStep(3);
+    dom.rawId.textContent = binToHex(rawId);
+    dom.publicKey.textContent = publicKey;
+
+    animation.run({
+        check: {
+            translateX: Animation.offsets.check.center,
+            opacity: 1,
+        },
+    });
+
+    scrollTo(".tutorial-step-3-container");
+    activateStep(3);
 }
 
 function setupAnimation(object) {
-  const svg = object.contentDocument.firstChild;
-  animation = new Animation(svg);
+    const svg = object.contentDocument.firstChild;
+    animation = new Animation(svg);
 }
 
 function setupListeners() {
-  document.querySelector(".tutorial-step-3-next").onclick = e => {
-    if (rawId) {
-      step4.trigger(rawId);
-      e.target.disabled = true;
-    }
-  };
+    document.querySelector(".tutorial-step-3-next").onclick = (e) => {
+        if (rawId) {
+            step4.trigger(rawId);
+            e.target.disabled = true;
+        }
+    };
 }
 
 export function init() {
-  onObjectLoad(dom.object, () => {
-    setupListeners();
-    setupAnimation(dom.object);
-    loadStep(3);
-  });
+    onObjectLoad(dom.object, () => {
+        setupListeners();
+        setupAnimation(dom.object);
+        loadStep(3);
+    });
 }
--- src/tutorial/webauthn.js
+++ src/tutorial/webauthn.js
@@ -1,72 +1,74 @@
-import { parseAttestationObject } from "../debugger/output-parser.js";
+import {
+    parseAttestationObject,
+    getUserPresentStatus,
+} from "../debugger/output-parser.js";
 
 import coseToJwk from "cose-to-jwk";
 
 export async function register(username, timeout) {
-  const challenge = new Uint8Array(32);
-  const userId = new Uint8Array(32);
-  crypto.getRandomValues(challenge);
-  crypto.getRandomValues(userId);
+    const challenge = new Uint8Array(32);
+    const userId = new Uint8Array(32);
+    crypto.getRandomValues(challenge);
+    crypto.getRandomValues(userId);
 
-  return navigator.credentials.create({
-    publicKey: {
-      rp: {
-        name: "Auth0 WebAuthn Playground"
-      },
-      user: {
-        id: userId,
-        name: username,
-        displayName: username
-      },
-      challenge: challenge,
-      pubKeyCredParams: [
-        {
-          type: "public-key",
-          alg: -7 // ES256
-        }
-      ],
-      timeout: timeout
-    }
-  });
+    return navigator.credentials.create({
+        publicKey: {
+            rp: {
+                name: "Auth0 WebAuthn Playground",
+            },
+            user: {
+                id: userId,
+                name: username,
+                displayName: username,
+            },
+            challenge: challenge,
+            pubKeyCredParams: [{
+                type: "public-key",
+                alg: -7, // ES256
+            }, ],
+            timeout: timeout,
+        },
+    });
 }
 
 export async function login(rawId, timeout) {
-  const challenge = new Uint8Array(32);
-  crypto.getRandomValues(challenge);
+    const challenge = new Uint8Array(32);
+    crypto.getRandomValues(challenge);
 
-  return navigator.credentials.get({
-    publicKey: {
-      challenge,
-      timeout,
-      allowCredentials: [
-        {
-          type: "public-key",
-          id: rawId
-        }
-      ]
-    }
-  });
+    return navigator.credentials.get({
+        publicKey: {
+            challenge,
+            timeout,
+            allowCredentials: [{
+                type: "public-key",
+                id: rawId,
+            }, ],
+        },
+    });
 }
 
 export function credentialsGetPublicKeyJWK(credentials) {
-  if (
-    !credentials ||
-    !credentials.response ||
-    !credentials.response.attestationObject
-  ) {
-    throw new Error("No public-key");
-  }
+    if (!credentials ||
+        !credentials.response ||
+        !credentials.response.attestationObject
+    ) {
+        throw new Error("No public-key");
+    }
 
-  const parsed = parseAttestationObject(credentials.response.attestationObject);
-  if (typeof parsed === "string") {
-    throw new Error("Error parsing attestationObject: ", parsed);
-  }
+    const parsed = parseAttestationObject(credentials.response.attestationObject);
+    if (typeof parsed === "string") {
+        throw new Error("Error parsing attestationObject: ", parsed);
+    }
 
-  try {
-    return coseToJwk(
-      parsed.authData.attestedCredentialData.credentialPublicKey
-    );
-  } catch (e) {
-    throw new Error("No public-key: ", e);
-  }
+    try {
+        return coseToJwk(
+            parsed.authData.attestedCredentialData.credentialPublicKey
+        );
+    } catch (e) {
+        throw new Error("No public-key: ", e);
+    }
+}
+
+export function isUserPresent(credentials) {
+    return getUserPresentStatus(credentials);
 }
