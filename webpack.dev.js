c2a7068144aa5c6c153a6da08cbe24634ffc0e9b
Sambego <sambellen@gmail.com>
2021-04-20 10:22:31 +0200

    Add browser support tast

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,14 +3,15 @@ const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: "development",
-  entry: {
-    index: "./src/index.js",
-    tutorial: "./src/tutorial/index.js",
-    debugger: "./src/debugger/index.js"
-  },
-  output: {
-    filename: "[name].js",
-    path: __dirname + "/dist/js"
-  }
+    mode: "development",
+    entry: {
+        index: "./src/index.js",
+        tutorial: "./src/tutorial/index.js",
+        debugger: "./src/debugger/index.js",
+        browserSupport: "./src/browser-support/index.js",
+    },
+    output: {
+        filename: "[name].js",
+        path: __dirname + "/dist/js",
+    },
 });
